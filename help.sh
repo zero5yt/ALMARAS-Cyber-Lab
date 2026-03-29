a68dcbdc95d5f09cb00681dff85aac04f75dc7d6
zero5yt <roderickalmaras05@gmail.com>
2026-03-29 20:45:53 +0800

    Create help.sh

---
+++ help.sh
@@ -0,0 +1,5 @@
+#!/data/data/com.termux/files/usr/bin/bash
+echo "--- QUICK START GUIDE ---"
+echo "SESSION 1 (Ngrok): termux-chroot && ./ngrok tcp --region ap 4444"
+echo "SESSION 2 (MSF):   proot-distro login ubuntu -- msfconsole -q"
+echo "SESSION 3 (Lab):   cd ~/ALMARAS-Cyber-Lab"
